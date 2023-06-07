a9f5e2bfdd3b940081dbc6a2d0d1bf26788e91c0
Erick384 <erickwachuma@gmail.com>
2023-06-07 14:38:54 +0300

    Uploading Newsletter-SignUp-Form commits

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-let userEmail = document.querySelector("#emails");
-let submitDetails = document.querySelector("#submit");
+function submitForm() {
+    // Get the input values
+    let userEmail = document.querySelector("#emails").value;
 
-// let users = [];
-
-submitDetails.addEventListener('submit', (e) => {
-    e.preventDefault();
-    userEmail.value = "";
-    let previousPage = document.querySelector(".container")
-    let success= `
-    <div class="container">
-    <img src="./assets/images/icon-success.svg" alt="" width="17px" height="17px"/>
-    </div>
-    `
-    previousPage.innerHTML = success;
-}); 
-
-// let acceptData = () => {
-//   let user = {
-//     userEmail: userEmail.value,
-//   };
-
-//   users.push(user); // Add the user to the array
-
-// };
+    // Create an object to hold the user details
+    let userDetails = {
+      userEmail: userEmail,
+    };
+    
+    // Save user details to local storage
+    localStorage.setItem("userDetails", JSON.stringify(userDetails));
+    
+    // Redirect to the new page
+    window.location.href = "success.html";
+  }
+function homePage() {
+    window.location.href = "index.html";
+}
